45793bf57a08d8355e32d5fe45156a2272ade0fb
Preetam Jinka <pj@isomero.us>
2014-10-04 01:12:08 -0400

    update benchmarks

--- udp_test.go
+++ udp_test.go
@@ -20,3 +20,18 @@ func TestUDP(t *testing.T) {
 		t.Errorf("expected destination port %v, got %v", 53, udpPacket.DestinationPort)
 	}
 }
+
+func BenchmarkUDP(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b := []byte{
+			238, 1, 0, 53, 0, 47, 5, 247,
+			253, 88, 1, 32, 0, 1, 0, 0, 0,
+			0, 0, 1, 6, 103, 111, 111, 103,
+			108, 101, 3, 99, 111, 109, 0, 0,
+			1, 0, 1, 0, 0, 41, 16, 0, 0, 0,
+			0, 0, 0, 0,
+		}
+
+		DecodeUDP(b)
+	}
+}
