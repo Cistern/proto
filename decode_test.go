44d31c063ce3f78497499b835d5fedafe6e4b1d6
Preetam Jinka <pj@isomero.us>
2014-10-04 01:52:37 -0400

    add benchmarks

--- decode_test.go
+++ decode_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestLayeredDecode(t *testing.T) {
+func TestMultilayerDecode(t *testing.T) {
 	b := []byte{
 		0, 15, 248, 20, 48, 0, 0, 37, 144, 82, 230, 31,
 		134, 221, 96, 0, 0, 0, 0, 40, 6, 64, 38, 32, 1,
@@ -36,5 +36,23 @@ func TestLayeredDecode(t *testing.T) {
 
 	if tcpPacket.HasACK() {
 		t.Error("ACK flag set when it should not be")
+	}
+}
+
+func BenchmarkMultilayerDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b := []byte{
+			0, 15, 248, 20, 48, 0, 0, 37, 144, 82, 230, 31,
+			134, 221, 96, 0, 0, 0, 0, 40, 6, 64, 38, 32, 1,
+			0, 80, 7, 0, 6, 0, 0, 0, 0, 0, 1, 0, 3, 38, 32,
+			1, 0, 80, 7, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 217,
+			104, 0, 80, 184, 89, 70, 22, 0, 0, 0, 0, 160, 2,
+			22, 128, 239, 131, 0, 0, 2, 4, 5, 160, 4, 2, 8,
+			10, 184, 73, 195, 65, 0, 0, 0, 0, 1, 3, 3, 7,
+		}
+
+		ethernetFrame := DecodeEthernet(b)
+		ipv6Packet := DecodeIPv6(ethernetFrame.Payload)
+		DecodeTCP(ipv6Packet.Payload)
 	}
 }
