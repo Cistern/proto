33ff3433081123b0bf2e86309595b82df2aeda24
Preetam Jinka <pj@isomero.us>
2014-10-23 19:17:29 -0400

    add IPv4 packet encoding

--- ipv4.go
+++ ipv4.go
@@ -68,3 +68,74 @@ func DecodeIPv4(b []byte) IPv4Packet {
 
 	return packet
 }
+
+func (p IPv4Packet) Bytes() []byte {
+	i := 0
+
+	var b []byte
+
+	if p.InternetHeaderLength > 5 {
+		b = make([]byte, (24)+len(p.Payload))
+	} else {
+		b = make([]byte, (20)+len(p.Payload))
+	}
+
+	b[i] = byte(p.Version)<<4 | byte(p.InternetHeaderLength)
+	i++
+
+	b[i] = byte(p.DSCP)<<2 | byte(p.ECN)
+	i++
+
+	b[i] = byte(p.Length >> 8)
+	b[i+1] = byte(p.Length)
+	i += 2
+
+	b[i] = byte(p.Identification >> 8)
+	b[i+1] = byte(p.Identification)
+	i += 2
+
+	b[i] = p.Flags<<5 | byte(p.FragmentationOffset>>8)
+	b[i+1] = byte(p.FragmentationOffset)
+	i += 2
+
+	b[i] = p.TimeToLive
+	i++
+
+	b[i] = p.Protocol
+	i++
+
+	b[i] = byte(p.HeaderChecksum >> 8)
+	b[i+1] = byte(p.HeaderChecksum)
+	i += 2
+
+	copy(b[i:], p.Source)
+	i += 4
+
+	copy(b[i:], p.Destination)
+	i += 4
+
+	if p.InternetHeaderLength > 5 {
+		copy(b[i:], p.Options)
+		i += 4
+	}
+
+	copy(b[i:], p.Payload)
+	i += len(p.Payload)
+
+	return b[:i]
+}
+
+func (p IPv4Packet) ComputeChecksum() uint16 {
+	sum := ((uint32(p.Version)<<4|uint32(p.InternetHeaderLength))<<8 |
+		(uint32(p.DSCP)<<2 | uint32(p.ECN))) +
+		uint32(p.Length) +
+		uint32(p.Identification) +
+		(uint32(p.Flags)<<13 | uint32(p.FragmentationOffset)) +
+		(uint32(p.TimeToLive)<<8 | uint32(p.Protocol)) +
+		(uint32(p.Source[0])<<8 | uint32(p.Source[1])) +
+		(uint32(p.Source[2])<<8 | uint32(p.Source[3])) +
+		(uint32(p.Destination[0])<<8 | uint32(p.Destination[1])) +
+		(uint32(p.Destination[2])<<8 | uint32(p.Destination[3]))
+
+	return uint16(0xffff) ^ (uint16(sum>>16) + uint16(sum))
+}
